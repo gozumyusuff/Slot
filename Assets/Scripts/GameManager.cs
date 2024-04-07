5bc7d36737f753aa4bbfc47b8c990f398c348d14
Yusuf Gözüm <139639043+gozumyusuff@users.noreply.github.com>
2024-04-07 16:29:40 +0300

    changing prefabs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField]
     private int boardHeight, boardWidth;
     [SerializeField]
     private GameObject[] colorIcons;
